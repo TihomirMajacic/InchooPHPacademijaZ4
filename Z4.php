c17b1a6c82a19b7afb4e3b63fb34c52e6c735840
TihomirMajacic <majacictihomir@gmail.com>
2019-01-28 15:49:56 +0100

    Kreiran izbornik

--- Z4.php
+++ Z4.php
@@ -1,6 +1,44 @@
 <?php
 
-class Zaposlenik{
+include_once 'Zaposlenik.php';
 
 
+$zaposlenici [] = array();
+
+$unos = 0;
+
+for (; ;) {
+    echo "
+    1. Pregled Zaposlenika\n
+    2. Unos novog Zaposlenika\n
+    3. Promjena podataka postojećem zaposleniku\n
+    4. Brisanje Zaposlenika\n
+    5. Statistika\n
+    ";
+    echo 'Unesi 1-5: ';
+    $unos = readline();
+
+    switch ($unos) {
+        case 1;
+            echo '';
+            break;
+
+        case 2;
+            echo '';
+            break;
+
+        case 3;
+            echo '';
+            break;
+
+        case 4;
+            echo '';
+            break;
+
+        case 5;
+            echo '';
+            break;
+
+
+    }
 }
